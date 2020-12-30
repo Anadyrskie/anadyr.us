15cb814e9de63d0d91431381857b1c39d3c9707f
Anadyrskie <anadyrskie@anadyr.us>
2020-12-29 20:25:06 -0700

    fix social-bar on android

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,26 @@
 /*Shy Navbar*/
-    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+const social_array = ["spotify", "youtube", "facebook", "instagram"];
+window.onscroll = function () {
+    let i;
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("social-bar").style.left = "1rem";
-} else {
-    document.getElementById("navbar").style.top = "-75px";
-    document.getElementById("social-bar").style.left = "-160px";
+    if (prevScrollpos <= currentScrollPos) {
+        document.getElementById("navbar").style.top = "-75px";
+        document.getElementById("social-bar").style.left = "-160px";
+        /*for (x of social_array) {
+            document.getElementById(x).style.left = "-160px";
+            console.log(x);
+        }*/
 
+    } else {
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("social-bar").style.left = "1rem";
+
+        /*for (x of social_array) {
+            document.getElementById(x).style.left = "";
+            console.log(x);
+        }*/
     }
     prevScrollpos = currentScrollPos;
 }
